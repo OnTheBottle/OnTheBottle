e1ede7d94c2490dcb36f40da4b5371bd4d31321a
Fabolo <fabolo1234@gmail.com>
2018-02-02 20:46:40 +0200

    Added basic structure of event creation

---
+++ message-subsystem-webapp/src/main/webapp/java/com/bottle/event/CreateEventServlet.java
@@ -0,0 +1,32 @@
+package com.bottle.event;
+
+import com.bottle.APIHandlerServlet;
+import com.bottle.event.service.EventRegistrationService;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+public class CreateEventServlet extends APIHandlerServlet.APIRequestHandler  {
+    private static final CreateEventServlet instance = new CreateEventServlet();
+
+    public static CreateEventServlet getInstance() {
+        return instance;
+    }
+
+    private CreateEventServlet() {
+    }
+
+    @Override
+    protected JSONStreamAware processRequest(HttpServletRequest request) {
+        Map<String, String[]> map = request.getParameterMap();
+        EventRegistrationService eventRegistrationService = EventRegistrationService.getEventRegistrationService();
+
+        JSONObject jsonObject = new JSONObject();
+        String result = eventRegistrationService.registrationEvent(map);
+        jsonObject.put("result", result);
+        return jsonObject;
+    }
+
+}
