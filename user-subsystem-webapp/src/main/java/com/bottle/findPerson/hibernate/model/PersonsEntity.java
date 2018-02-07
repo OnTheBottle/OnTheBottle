9c202359f744c8670640a57445ebac90f4c35fc4
Daniel <cpr.dunn@gmail.com>
2018-02-07 22:22:32 +0200

    connected hibernate, add entities(local,tempered)

---
+++ user-subsystem-webapp/src/main/java/com/bottle/findPerson/hibernate/model/PersonsEntity.java
@@ -0,0 +1,89 @@
+package com.bottle.findPerson.hibernate.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons", schema = "find_person_flow")
+public class PersonsEntity {
+    private int personId;
+    private String name;
+    private String secondName;
+    private String city;
+    private String region;
+
+    @Id
+    @Column(name = "person_id", nullable = false)
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "second_name", nullable = true, length = 45)
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Basic
+    @Column(name = "city", nullable = true, length = 45)
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Basic
+    @Column(name = "region", nullable = true, length = 45)
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonsEntity that = (PersonsEntity) o;
+
+        if (personId != that.personId) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (secondName != null ? !secondName.equals(that.secondName) : that.secondName != null) return false;
+        if (city != null ? !city.equals(that.city) : that.city != null) return false;
+        if (region != null ? !region.equals(that.region) : that.region != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = personId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (secondName != null ? secondName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (region != null ? region.hashCode() : 0);
+        return result;
+    }
+}
