dff035b81c422e40f8af5997a50e549c6f355fa7
SergeyPensov <Akumi_666>
2018-03-26 21:22:43 +0300

    merge find person, relationship and event

---
+++ message-subsystem-webapp/src/main/java/com/bottle/event/model/entity/Place.java
@@ -0,0 +1,32 @@
+package com.bottle.event.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "events")
+@ToString(exclude = "events")
+public class Place {
+    @Id
+    @Column(columnDefinition = "BINARY(16)", name = "place_id")
+    private UUID id;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name="Event_Places",
+            joinColumns={@JoinColumn(name="place_id")},
+            inverseJoinColumns={@JoinColumn(name="event_id")}
+    )
+    private Set<Event> events = new HashSet<>();
+
+    public void addPlaceToAllEvents() {
+        events.forEach(event -> event.getPlaces().add(this));
+    }
+}
---
+++ user-subsystem-webapp/src/main/java/com/bottle/findPerson/MainClass.java
@@ -0,0 +1,17 @@
+package com.bottle.findPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("com.bottle.findPerson")
+@EntityScan("com.bottle.findPerson")
+@ComponentScan("com.bottle.findPerson")
+public class MainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
