c2a8113e6bb68e9e3fd13ffaa0d49555294101bf
Fabolo <fabolo1234@gmail.com>
2018-03-26 21:33:20 +0300

    Added validator of Event

--- message-subsystem-webapp/src/main/java/com/bottle/event/model/DTO/EventDTO.java
+++ message-subsystem-webapp/src/main/java/com/bottle/event/model/DTO/EventDTO.java
@@ -2,30 +2,17 @@ package com.bottle.event.model.DTO;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 @Data
 public class EventDTO {
     private String id;
-
-    @NotNull
     private String title;
-
     private String text;
-
-    @NotNull
     private String startTime;
-
-    @NotNull
     private String endTime;
-
-    @NotNull
     private String place;
-
     private List<UUID> users;
-
-    @NotNull
     private String owner;
 }
