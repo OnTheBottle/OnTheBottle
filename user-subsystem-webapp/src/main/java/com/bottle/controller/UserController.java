2b3cf1ccab2c8d79611da95691853e8be331eebd
Fabolo <fabolo1234@gmail.com>
2018-05-14 18:48:14 +0300

    Start work with users info in event

--- user-subsystem-webapp/src/main/java/com/bottle/controller/UserController.java
+++ user-subsystem-webapp/src/main/java/com/bottle/controller/UserController.java
@@ -2,9 +2,12 @@ package com.bottle.controller;
 
 
 import com.bottle.model.dto.UserDTO;
+import com.bottle.model.dto.request.UsersIdDTO;
+import com.bottle.service.AuthService;
 import com.bottle.service.UserService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +18,15 @@ import java.util.UUID;
 @RestController
 @RequestMapping(path = "/user")
 @NoArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     private UserService userService;
+    private AuthService authService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @RequestMapping(path = "/get_by_id", method = RequestMethod.POST)
@@ -35,4 +40,19 @@ public class UserController {
         return userService.getUsers();
     }
 
+    @RequestMapping(path = "/getUsersInfo", method = RequestMethod.POST)  //TODO
+    public ResponseEntity<?> leaveUserEvent(@RequestBody UsersIdDTO usersIdDTO,
+                                            @RequestParam(name = "access_token") String token) {
+        if (!authService.isValidToken(token)) return getNonValidTokenResponse();
+
+        usersIdDTO.getUsers().forEach(System.out::println);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> getNonValidTokenResponse() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-type", "text/plain");
+        String message = "Non-valid token";
+        return new ResponseEntity<>(message, headers, HttpStatus.BAD_REQUEST);
+    }
 }
---
+++ user-subsystem-webapp/src/main/java/com/bottle/model/dto/request/UserIdDTO.java
@@ -0,0 +1,10 @@
+package com.bottle.model.dto.request;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class UserIdDTO {
+    UUID id;
+}
