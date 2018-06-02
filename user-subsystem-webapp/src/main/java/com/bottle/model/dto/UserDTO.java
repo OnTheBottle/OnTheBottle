9edbb64605fd7c40c58da96d8494f3d8a8090cd1
ctaxx <bykov.stanislav.p@gmail.com>
2018-06-02 20:16:56 +0300

    people-search works better

--- user-subsystem-webapp/src/main/java/com/bottle/model/dto/UserDTO.java
+++ user-subsystem-webapp/src/main/java/com/bottle/model/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.bottle.model.dto;
 
+import com.bottle.entity.User;
 import lombok.*;
 
 import java.util.List;
@@ -18,4 +19,53 @@ public class UserDTO {
     private String country;
     private List<UUID> friendsId;
     private String friendStatus;
+
+    public static Builder newBuilder() {
+        return new UserDTO().new Builder();
+    }
+
+    public class Builder {
+
+        private Builder() {
+        }
+
+        public UserDTO build() {
+            return UserDTO.this;
+        }
+
+        public Builder setId(UUID id) {
+            UserDTO.this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            UserDTO.this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            UserDTO.this.surname = surname;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            UserDTO.this.age = age;
+            return this;
+        }
+
+        public Builder setAvatarUrl(String avatarUrl) {
+            UserDTO.this.avatarUrl = avatarUrl;
+            return this;
+        }
+
+        public Builder setCity(String city) {
+            UserDTO.this.city = city;
+            return this;
+        }
+
+        public Builder setCountry(String country) {
+            UserDTO.this.country = country;
+            return this;
+        }
+    }
 }
